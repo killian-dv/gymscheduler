6c8623b0569827a25d5e2564234f3515c2281352
killian-dv <killian.david@etudiant.univ-reims.fr>
2023-04-20 16:36:14 +0200

    ajout select

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import TrainingDay from "./components/TrainingDay.svelte";
+</script>
+<style>
+    * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+        font-family: 'Rubik', sans-serif;
+    }
+    main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 1rem;
+        height: 100vh;
+    }
+    h1 {
+        color: #fb771a;
+    }
+    h1, h2, h3 {
+        text-align: center;
+    }
+</style>
+<main>
+    <h1>GymScheduler</h1>
+    <p>Voici ma première app avec Svelte</p>
+    <TrainingDay />
+</main>
---
+++ src/routes/components/TrainingDay.svelte
@@ -0,0 +1,22 @@
+<script>
+    let nbOfDays = [
+        {id: 1, text: "1 jour", value: [1]},
+        {id: 2, text: "2 jours", value: [1, 2]},
+        {id: 3, text: "3 jours", value: [1, 2, 3]},
+        {id: 4, text: "4 jours", value: [1, 2, 3, 4]},
+        {id: 5, text: "5 jours", value: [1, 2, 3, 4, 5]},
+        {id: 6, text: "6 jours", value: [1, 2, 3, 4, 5, 6]},
+        {id: 7, text: "7 jours", value: [1, 2, 3, 4, 5, 6, 7]},
+    ];
+    let nbDaysTrainingPerWeek;
+
+</script>
+
+<form>
+    <select bind:value={nbDaysTrainingPerWeek}>
+    {#each nbOfDays as day (day.id)}
+        <option value={day}>{day.text} par semaine</option>
+    {/each}
+    </select>
+</form>
+{JSON.stringify(nbDaysTrainingPerWeek)}
